b016002a60faa5ad272672e45d17c132c2290a8e
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-01-27 00:35:22 -0300

    Update utils.cpp

--- RISC-V DISASSEMBLER + SIMULATOR/utils.cpp
+++ RISC-V DISASSEMBLER + SIMULATOR/utils.cpp
@@ -5,31 +5,44 @@
 
 //A funcao a seguir serve para fazer extensao de sinal de um campo de bits
 int bitSigner(unsigned int field, unsigned int size){
-    /*INSERE CODIGO AQUI*/
-    return 0;
+    int value = field;
+    value = ((value << (32 - size)) >> (32 - size));
+    return value;
 }
 
 /*AS FUNCOES DE OFFSET DEVEM RETORNAR O TAMANHO DO OFFSET EM BYTES*/
 int get_branch_offset(Instruction instruction) {
-  /* YOUR CODE HERE */
-  return 0;
+  int offset = 0x00000000;
+  offset = offset | (0x0000001e & instruction.sbtype.imm5);
+  offset = offset | (0x000007e0 & (instruction.sbtype.imm7 << 5));
+  offset = offset | (0x00000800 & (instruction.sbtype.imm5 << 11));
+  offset = offset | (0x00001000 & (instruction.sbtype.imm7 << 6));
+  return bitSigner(offset, 13);
 }
 
 
 int get_jump_offset(Instruction instruction) {
-  /* YOUR CODE HERE */
-  return 0;
+  int offset = 0x00000000;
+	offset |= (instruction.ujtype.imm >> 8) & 0x000007fe; // imm[1:10]
+	offset |= (instruction.ujtype.imm << 3) & 0x00000800; // imm[11]
+	offset |= (instruction.ujtype.imm << 12) & 0x000ff000; // imm[12:19]
+	offset |= (instruction.ujtype.imm << 2) & 0x00100000;// imm[20]
+	return bitSigner(offset, 21);
 }
 
 
 int get_store_offset(Instruction instruction) {
-  /* YOUR CODE HERE */
-  return 0;
+  int offset = 0x00000000;
+  offset = offset | instruction.stype.imm7;
+  offset = offset << 5;
+  offset = offset | instruction.stype.imm5;
+  return bitSigner(offset, 12);
 }
 
 
 void handle_invalid_instruction(Instruction instruction) {
   printf("Invalid Instruction: 0x%08x\n", instruction.bits);
+  return;
 }
 
 
